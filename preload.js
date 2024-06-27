835879b5f6bb79ffb671ca2da4aacbeee435abc8
panqike <panqike@gmail.com>
2024-06-26 22:46:11 -0400

    remove unused starter code from preload bridge

--- preload.js
+++ preload.js
@@ -1,11 +1,5 @@
 const { contextBridge, ipcRenderer } = require('electron')
 
-contextBridge.exposeInMainWorld('versions', {
-  node: () => process.versions.node,
-  chrome: () => process.versions.chrome,
-  electron: () => process.versions.electron,
-});
-
 contextBridge.exposeInMainWorld('utils', {
     convert: (image, options) => ipcRenderer.invoke('convert', image, options),
     liquid: (file, options) => ipcRenderer.invoke('liquid', file, options),
