681c8abeaf5d0106b75dc7de60c5f18248378bdd
panqike <panqike@gmail.com>
2024-07-07 16:38:15 -0400

    add size form, update form markup and close dialog on submit

--- src/js/settings.js
+++ src/js/settings.js
@@ -49,7 +49,7 @@ export default function settings() {
 
   deleteButtons.forEach(button => {
     button.addEventListener('click', e => {
-      settings.unset(button.getAttribute('data-size'));
+      storage.unset(button.getAttribute('data-size'));
       button.closest('tr').remove();
     })
   });
@@ -99,15 +99,39 @@ export default function settings() {
     // TO DO Validate input
     const feedback = document.getElementById('add-size--feedback');
     let size = document.getElementById('add-size--size').value.replaceAll('px', '');
-    let key = size.indexOf('x') == -1 ? `settings.scale.${size}` : `settings.resize.${size}`;
+    let group = size.indexOf('x') == -1 ? 'scale' : 'resize';
+    let key = `settings.${group}.${size}`;
     let val = document.getElementById('add-size--selected').checked;
+    let table = document.querySelector(`#table-${group} tbody`);
 
     feedback && (feedback.innerHTML = '&nbsp;');
     try {
       storage.update(key, val);
-      feedback && (feedback.textContent = 'New custom size added.');
+      let tr = document.createElement('tr');
+      tr.className = 'added-new';
+      let out = `<th scope="row" id="size-${size}">${size}</th>` +
+      `<td><input type="checkbox" class="toggle" id="size-${size}-selected" name="resize-selected" ${val ? 'checked' : ''} aria-label="Checked by default" aria-describedby="size-${size}" data-size="settings.resize.${size}"></td>` +
+      `<td><button class="delete" aria-label="Delete size" title="Delete" type="button" data-size="settings.resize.${size}">
+        <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 512 512">
+        <path fill="none" d="M296 64h-80a7.91 7.91 0 0 0-8 8v24h96V72a7.91 7.91 0 0 0-8-8" />
+        <path fill="none" d="M292 64h-72a4 4 0 0 0-4 4v28h80V68a4 4 0 0 0-4-4" />
+        <path fill="currentColor" d="M447.55 96H336V48a16 16 0 0 0-16-16H192a16 16 0 0 0-16 16v48H64.45L64 136h33l20.09 314A32 32 0 0 0 149 480h214a32 32 0 0 0 31.93-29.95L415 136h33ZM176 416l-9-256h33l9 256Zm96 0h-32V160h32Zm24-320h-80V68a4 4 0 0 1 4-4h72a4 4 0 0 1 4 4Zm40 320h-33l9-256h33Z" />
+        </svg></button></td>`;
+      tr.innerHTML += out;
+      tr.querySelector('.delete').addEventListener('click', e => {
+        storage.unset(e.currentTarget.getAttribute('data-size'));
+        tr.remove();
+      })
+      table.appendChild(tr);
+      setTimeout(() => {
+        tr.classList.remove('added-new');
+      }, 2500)
+      addSizeForm.reset();
+      e.currentTarget.closest('dialog').close();
+
     } catch (err) {
       feedback && (feedback.textContent = 'There was a problem adding the size');
+      console.error(err);
     }
   });
 }
